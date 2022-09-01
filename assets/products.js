68f89a2debeb101d18c865aa76c95f27016683ef
Leo <leomutungi19@gmail.com>
2022-09-01 12:45:34 +0300

    node

---
+++ assets/products.js
@@ -0,0 +1,9 @@
+// function getProducts(){
+//     fetch('https://surveys/:survey_id/responses').then((response) => response.json()).then((data) => {
+//         console.log(data);
+//     }).catch((err) => {
+//         console.log("products not found", err);
+//     });
+// }
+
+// getProducts()
