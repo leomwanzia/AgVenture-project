19601c0c7e9a20539e5c9cc50a47749524441e6f
Leo <leomutungi19@gmail.com>
2022-09-01 15:54:15 +0300

    js

--- assets/products.js
+++ assets/products.js
@@ -1,10 +1,10 @@
-// function getProducts(){
-//     fetch('https://surveys/:survey_id/responses').then((response) => response.json()).then((data) => {
-//         console.log(data);
-//     }).catch((err) => {
-//         console.log("products not found", err);
-//     });
-// }
+function getProducts(){
+    fetch("https://my-json-server.typicode.com/leomwanzia/AgVenture").then((response) => response.json()).then((data) => {
+        console.log(data);
+    }).catch((err) => {
+        console.log("products not found", err);
+    });
+}
 
-// getProducts()
+getProducts()
 
